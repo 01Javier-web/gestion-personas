26d90a05c428e0bc93c8769ac4064b826b1d9f4a
Javier Ortiz <andres13ytoo@gmail.com>
2026-07-02 15:29:41 -0500

    Metodo constructor

--- class/persona.php
+++ class/persona.php
@@ -6,6 +6,14 @@ class persona
   public $edad;
   public $correo;
   public $apellido;
+
+  public function __construct($nombre = "", $apellido = "", $edad = 0, $correo = "")
+  {
+    $this->nombre = $nombre;
+    $this->apellido = $apellido;
+    $this->edad = $edad;
+    $this->correo = $correo;
+  }
 public function saludar(){
   
 return "Mi nombre es " . $this->nombre. " ".$this->apellido. " y tengo " . $this->edad . " años. Mi correo es: " . $this->correo. "<br>";
--- public/index.php
+++ public/index.php
@@ -2,18 +2,11 @@
 
 require_once "../class/persona.php";
 
-$persona1 = new persona();
+$persona1 = new persona("Javier", "Ortiz", 17, "andres13ytoo@gmail.com");
+$persona2 = new persona("María", "Ossa", 17, "mariaossa@gmail.com");
 
-$persona1->nombre = "Javier";
-$persona1->apellido = "Ortiz";
-$persona1->edad = 17;
-$persona1->correo = "andres13ytoo@gmail.com";
-
-$persona2 = new persona();
-$persona2->nombre = "María";
-$persona2->apellido = "Ossa";
-$persona2->edad = 17;
-$persona2->correo = "mariaossa@gmail.com";
 
 echo $persona1->saludar();
 echo $persona2->saludar();
+
+
