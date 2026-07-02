06a6eb8596d08d9e1f4ff187a659745fe81c82d8
Javier Ortiz <andres13ytoo@gmail.com>
2026-07-02 15:07:33 -0500

    palabras reservadas this

---
+++ class/persona.php
@@ -0,0 +1,18 @@
+<?php
+
+class persona
+{
+  public $nombre;
+  public $edad;
+  public $correo;
+  public $apellido;
+public function saludar(){
+  
+return "Mi nombre es " . $this->nombre. " ".$this->apellido. " y tengo " . $this->edad . " años. Mi correo es: " . $this->correo. "<br>";
+}
+
+
+
+      
+}
+?>
--- public/index.php
+++ public/index.php
@@ -2,6 +2,18 @@
 
 require_once "../class/persona.php";
 
-$Javier = new persona();
+$persona1 = new persona();
 
-$Javier->saludar();
+$persona1->nombre = "Javier";
+$persona1->apellido = "Ortiz";
+$persona1->edad = 17;
+$persona1->correo = "andres13ytoo@gmail.com";
+
+$persona2 = new persona();
+$persona2->nombre = "María";
+$persona2->apellido = "Ossa";
+$persona2->edad = 17;
+$persona2->correo = "mariaossa@gmail.com";
+
+echo $persona1->saludar();
+echo $persona2->saludar();
